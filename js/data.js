4e1712d83f4c8d94b58ad787297ec5c7844d6129
Your Name <zubr0009@algonquinlive.com>
2023-01-27 20:49:09 -0500

    added files for first hybrid phase

---
+++ js/data.js
@@ -0,0 +1,53 @@
+const songList = [
+    {
+        artist: 'Kelsey Lamb',
+        title: 'Heartbreak Away',
+        track: 'heartbreak-away-official-music-video.mp3',
+        thumbnail: 'heartbreak-away-thumb.jpeg',
+        large: 'heartbreak-away-full.jpeg',
+    },
+    {
+        artist: 'Home Free',
+        title: 'Understand',
+        track: 'understand.mp3',
+        thumbnail: 'understand-thumb.jpeg',
+        large: 'understand-full.jpeg',
+    },
+    {
+        artist: 'Kelsey Lamb',
+        title: 'When I Remember You',
+        track: 'when-i-remember-you-acoustic.mp3',
+        thumbnail: 'when-i-remember-you-thumb.jpeg',
+        large: 'when-i-remember-you-full.jpeg',
+    },
+    {
+        artist: 'Ashley Cooke',
+        title: 'Under',
+        track: 'under-official-lyric-video.mp3',
+        thumbnail: 'under-thumb.jpeg',
+        large: 'under-full.jpeg',
+    },
+    {
+        artist: 'Ashley Cooke',
+        title: 'Back in the Saddle',
+        track: 'back-in-the-saddle-official-music-video.mp3',
+        thumbnail: 'back-in-the-saddle-thumb.jpeg',
+        large: 'back-in-the-saddle-full.jpeg',
+    },
+    {
+        artist: 'Celtic Woman',
+        title: 'Going Home',
+        track: 'celtic-woman-going-home.mp3',
+        thumbnail: 'going-home-thumb.jpeg',
+        large: 'going-home-full.jpeg',
+    },
+    {
+        artist: 'Home Free',
+        title: 'Listen to the Music',
+        track: 'placeholder.mp3',
+        thumbnail: 'listen-to-the-music-thumb.jpeg',
+        large: 'listen-to-the-music-full.jpeg',
+    },
+];
+
+export default songList;
